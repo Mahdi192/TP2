cc4fad0a9d7620904ab072bbd1f77f718400173a
Mahdi192 <e6249382@cmaisonneuve.qc.ca>
2024-05-19 23:41:21 -0400

    "Creation de la classe DonneesGeo"

--- git.py
+++ git.py
@@ -1,2 +1,15 @@
-print("TEST 2")
+import csv
+import json
+from math import radians, sin, cos, sqrt, atan2
+
+class donneesgeo:
+    def __init__(self, ville, pays, latitude, longitude):
+        self.ville = ville
+        self.pays = pays
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+
+    def __str__(self):
+        return f"{self.ville}, {self.pays}, {self.latitude}, {self.longitude}"
+
 
