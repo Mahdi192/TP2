af138e95c0ae0d037ed52b7ee37c4cb54c4fd426
Mahdi192 <e6249382@cmaisonneuve.qc.ca>
2024-05-20 14:10:28 -0400

    Creation fonction lire_donnees_csv et donnees.csv

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ import csv
 import json
 from math import radians, sin, cos, sqrt, atan2
 
-class donneesgeo:
+class donnees_geo:
     def __init__(self, ville, pays, latitude, longitude):
         self.ville = ville
         self.pays = pays
@@ -11,5 +11,22 @@ class donneesgeo:
 
     def __str__(self):
         return f"{self.ville}, {self.pays}, {self.latitude}, {self.longitude}"
+
+
+def lire_donnees_csv():
+    donnees = []
+    with open('donnees.csv', 'r', encoding='utf-8') as f:
+        lecteur_csv = csv.reader(f)
+        next(lecteur_csv)  
+        for ligne in lecteur_csv:
+            ville, pays, latitude, longitude = ligne
+            donnee = donnees_geo(ville, pays, latitude, longitude)
+            donnees.append(donnee)
+    return donnees
+
+
+donnees = lire_donnees_csv()
+for donnee in donnees:
+    print(donnee)
 
 
